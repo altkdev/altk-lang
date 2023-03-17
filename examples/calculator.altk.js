8c2329da4c0b996baba3878325a16882bf6ce398
Axel Fleming <84058356+dr-comeemeememem@users.noreply.github.com>
2023-03-17 12:35:15 -0600

    Update calculator.altk.js
    Signed-off-by: Axel Fleming <84058356+dr-comeemeememem@users.noreply.github.com>

--- examples/calculator.altk.js
+++ examples/calculator.altk.js
@@ -1,12 +1,9 @@
 while (1) {
-  // 0 repeats forever
   var num1 = prompt("what is the first number")
   var num2 = prompt("what is the second number")
   var result = num1 + num2
   alert("The result is" + result)
   var exit = confirm("do you want to exit?")
-  if (exit = true) {
-    continue
-  }
+  if (exit) continue;
 }
   
